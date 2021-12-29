0564d6f31e000f66112903e1f2bfc5d819a280dd
Pedro <pj.nunes@campus.fct.unl.pt>
2021-12-29 20:28:40 +0000

    Work in progress - not working

---
+++ src/main/java/org/jabref/logic/importer/GoogleScholarProfilesParser.java
@@ -0,0 +1,59 @@
+package org.jabref.logic.importer;
+
+import com.google.gson.*;
+import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.field.StandardField;
+
+public class GoogleScholarProfilesParser {
+
+    JsonObject jsonObject;
+
+    public GoogleScholarProfilesParser(String jsonString) {
+        jsonObject = new Gson().fromJson(jsonString, JsonObject.class).getAsJsonObject();
+    }
+
+    public BibEntry parseEntries() {
+
+        BibEntry bibEntry = new BibEntry();
+
+        if (jsonObject.has("profiles")) {
+            JsonArray jsonArray = jsonObject.get("profiles").getAsJsonArray();
+
+            for (JsonElement jsonElement : jsonArray
+            ) {
+                JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+                if (jsonObject.has("name")) {
+                    bibEntry.setField(StandardField.AUTHOR_NAME, jsonObject.get("name").getAsString());
+                }
+
+                if (jsonObject.has("affiliations")) {
+                    bibEntry.setField(StandardField.AFFILIATION, jsonObject.get("affiliations").getAsString());
+                }
+
+                if (jsonObject.has("email")) {
+                    bibEntry.setField(StandardField.EMAIL, jsonObject.get("email").getAsString());
+                }
+
+                if (jsonObject.has("interests")) {
+                    JsonArray jsonArray2 = jsonObject.get("interests").getAsJsonArray();
+
+                    for (JsonElement jsonElement2 : jsonArray2
+                    ) {
+                        JsonObject jsonObject2 = jsonElement2.getAsJsonObject();
+
+                        if (jsonObject2.has("title")) {
+                            bibEntry.setField(StandardField.INTERESTS, jsonObject.get("INTERESTS").getAsString());
+                        }
+                    }
+                }
+
+
+            }
+
+        }
+
+        return bibEntry;
+
+    }
+}
--- src/main/java/org/jabref/logic/importer/fetcher/GoogleScholarProfiles.java
+++ src/main/java/org/jabref/logic/importer/fetcher/GoogleScholarProfiles.java
@@ -2,26 +2,35 @@ package org.jabref.logic.importer.fetcher;
 
 import com.google.gson.JsonObject;
 
+import org.jabref.logic.importer.GoogleScholarProfilesParser;
+import org.jabref.model.entry.BibEntry;
 import serpapi.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class GoogleScholarProfiles {
-    public GoogleScholarProfiles() {
-        Map<String, String> parameter = new HashMap<>();
 
+    Map<String, String> parameter = new HashMap<>();
+
+    public GoogleScholarProfiles(String query) {
         parameter.put("engine", "google_scholar_profiles");
-        parameter.put("mauthors", "Mike");
+        parameter.put("mauthors", query);
         parameter.put("api_key", "83c392e3fe916c0ef08910e17d9ab0e07058b2be37e8121c3fa806514c2bb37d");
+    }
 
+    public BibEntry executeQuery() {
         GoogleSearch search = new GoogleSearch(parameter);
+        GoogleScholarProfilesParser parser = null;
 
         try {
             JsonObject results = search.getJson();
-            var profiles = results.get("profiles");
+            parser = new GoogleScholarProfilesParser(results.getAsString());
         } catch (
                 SerpApiSearchException ex) {
+            System.out.println(ex);
         }
+        return parser.parseEntries();
     }
+
 }
