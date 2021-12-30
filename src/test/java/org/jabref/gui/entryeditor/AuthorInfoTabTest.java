d4e3bf01f77a9ed326d8f830c209cab61ee8f90d
Bruna <b.arroja@campus.fct.unl.pt>
2021-12-30 17:05:26 +0000

    Modified the test class for the authorInfoTab.

--- src/test/java/org/jabref/gui/entryeditor/AuthorInfoTabTest.java
+++ src/test/java/org/jabref/gui/entryeditor/AuthorInfoTabTest.java
@@ -1,70 +1,41 @@
 package org.jabref.gui.entryeditor;
 
-import org.jabref.gui.DialogService;
-import org.jabref.gui.StateManager;
-import org.jabref.gui.autocompleter.SuggestionProviders;
-import org.jabref.gui.externalfiletype.ExternalFileTypes;
-import org.jabref.gui.util.TaskExecutor;
-import org.jabref.logic.journals.JournalAbbreviationRepository;
-import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
-import org.jabref.model.entry.BibEntryTypesManager;
+import org.jabref.model.entry.BibEntryType;
+import org.jabref.model.entry.field.BibField;
 import org.jabref.model.entry.field.Field;
+import org.jabref.model.entry.field.OrFields;
 import org.jabref.model.entry.field.StandardField;
-import org.jabref.preferences.PreferencesService;
+import org.jabref.testutils.category.GUITest;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.testfx.framework.junit5.ApplicationExtension;
 
-import javax.swing.undo.UndoManager;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
+@GUITest
+@ExtendWith(ApplicationExtension.class)
 class AuthorInfoTabTest {
 
-    AuthorInfoTab info;
-    private BibDatabaseContext databaseContext = mock(BibDatabaseContext.class);
-    private SuggestionProviders suggestionProviders = mock(SuggestionProviders.class);
-    private UndoManager undoManager = mock(UndoManager.class);
-    private DialogService dialogService = spy(DialogService.class);
-    private PreferencesService preferences =  mock(PreferencesService.class);
-    private StateManager stateManager = mock(StateManager.class);
-    private BibEntryTypesManager entryTypesManager = mock(BibEntryTypesManager.class);
-    private ExternalFileTypes externalFileTypes = mock(ExternalFileTypes.class);
-    private TaskExecutor taskExecutor = mock(TaskExecutor.class);
-    private JournalAbbreviationRepository journalAbbreviationRepository = mock(JournalAbbreviationRepository.class);
-    private BibEntry bibEntry;
+    private BibEntryType bibEntryType=new BibEntryType(BibEntry.DEFAULT_TYPE, new LinkedHashSet<BibField>(), new LinkedHashSet<OrFields>());
 
     @BeforeAll
     public static void setupAll() {
         System.out.println("Starting the tests!");
     }
 
-    @BeforeEach
-    public void setup() {
-
-        info = new AuthorInfoTab( databaseContext,
-                 suggestionProviders,
-                 undoManager,
-                 dialogService,
-                 preferences,
-                 stateManager,
-                 entryTypesManager,
-                 externalFileTypes,
-                 taskExecutor,
-                 journalAbbreviationRepository);
-    }
-
     //@DisplayName()
     @ParameterizedTest
     @MethodSource ("expectedFields")
     public void shouldDetermineFields() {
-        Set<Field> receivedFields = info.determineFieldsToShow(bibEntry);
+        Set<Field> receivedFields = bibEntryType.getAuthorInfoFields();
         assertFalse(receivedFields.isEmpty());
         assertEquals(4, receivedFields.size());
     }
@@ -77,8 +48,7 @@ class AuthorInfoTabTest {
         expectedFields.add(StandardField.INTERESTS);
 
 
-       return expectedFields;
+        return expectedFields;
     }
 
-    //{StandardField.AUTHOR_NAME, StandardField.AFFILIATION, StandardField.EMAIL, StandardField.INTERESTS}
 }
